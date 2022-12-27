7b836cec152ad6604840fab160bd22a91eef27e7
adithya-yadav <badriadithya7396@gmail.com>
2022-12-27 11:28:38 +0530

    Doing Network calls and andling response the right way

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
-import { useSelector } from 'react-redux';
+import { Fragment, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+import { uiActions } from './store/ui-slice';
+
+let isInitial = true;
 
 function App() {
   const showToggleCart = useSelector(state=>state.ui.cartIsVisible)
+  const selectCart = useSelector(state=>state.cart)
+  const selectNotification = useSelector(state=>state.ui.notification)
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    async function sendingPutRequest(){
+      dispatch(uiActions.addNotification("sending"))
+      const response =await fetch(`https://quotes-21aa6-default-rtdb.firebaseio.com/cart.json`,{
+        method:'PUT',
+        body:JSON.stringify(selectCart),
+        headers:{
+          'content-type':'application/json'
+        }
+      })
+      if(response.ok){
+        dispatch(uiActions.addNotification("success"))
+      }else{
+        throw new Error("sending Request is failed")
+      }
+    }
+    if(isInitial){
+      isInitial=false
+      return
+    }
 
+    sendingPutRequest().catch((error)=>{
+      dispatch(uiActions.addNotification("error"))
+    })
+  },[selectCart,dispatch])
   return (
+    <Fragment>
+      {selectNotification.status && <Notification status={selectNotification.status} title={selectNotification.title} message={selectNotification.message} />}
     <Layout>
       {showToggleCart && <Cart />}
       <Products />
     </Layout>
+    </Fragment>
   );
 }
 
--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -4,11 +4,31 @@ const uiSlice = createSlice({
   name: "ui",
   initialState: {
     cartIsVisible: false,
+    notification:{
+        status:null,
+        title:null,
+        message:null
+    }
   },
   reducers: {
     toggle(state) {
       state.cartIsVisible = !state.cartIsVisible;
     },
+    addNotification(state,action){
+        if(action.payload === 'sending'){
+            state.notification.status = action.payload
+            state.notification.title = "Sending..."
+            state.notification.message = "Sending cart data!"
+        }else if(action.payload === "success"){
+            state.notification.status = action.payload
+            state.notification.title = "Success!"
+            state.notification.message = "Sending cart data successfully!"
+        }else{
+            state.notification.status = action.payload
+            state.notification.title = "Error!"
+            state.notification.message = "Sending cart data failed!"
+        }
+    }
   },
 });
 
